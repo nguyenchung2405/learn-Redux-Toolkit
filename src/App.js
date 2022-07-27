1988aa768ba361c3d5c44e0cb5dd9b09ff0a9bc1
nguyenchung2405 <chungdaik@gmail.com>
2022-07-27 16:41:05 +0700

    hoc redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Cartcontainer from "./components/cartContainer/CartContainer";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return <main>
     <Navbar/>
+    <Cartcontainer/>
   </main>;
 }
 export default App;
