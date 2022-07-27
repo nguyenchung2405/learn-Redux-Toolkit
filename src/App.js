c2e3be4644456b7de25d7e59c135da2df53601b0
nguyenchung2405 <chungdaik@gmail.com>
2022-07-27 16:15:49 +0700

    hoc redux toolkit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return <main>
+    <Navbar/>
+  </main>;
+}
+export default App;
---
+++ src/redux/features/cart/cartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+import CartItems from "../../../cartItems";
+
+const initialState = {
+    cartItems: CartItems,
+    amount:0,
+    total:0,
+    isLoading: true
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState
+});
+// console.log(cartSlice)
+export default cartSlice.reducer;
